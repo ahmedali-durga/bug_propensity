8ddc28cfa7baf347662ae5a7e07a246566eaf739
Ahmedali Durga <ahmedali.durga@live.in>
2016-03-26 20:00:17 +0530

    Update setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Ahmedali Durga',
     author_email='ahmedali.durga@live.in',
     url='https://github.com/ahmedali-durga/bug_propensity',
-    download_url='https://github.com/ahmedali-durga/bug_propensity/tarball/0.1',
+    download_url='https://github.com/ahmedali-durga/bug_propensity/tarball/0.2',
     keywords=['bug', 'propensity', 'bug-propensity'],
     classifiers=[],
 )
